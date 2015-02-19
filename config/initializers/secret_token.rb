8afaf234c68120bfcf14c8c36d4418f6089172b4
krjw92 <krjw92@kaist.ac.kr>
2015-02-20 00:56:09 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kaistuhak::Application.config.secret_key_base = '2fd242d4b385dc1c37ccb45d6e324042b33bf3bbb85620fdd39dfdddcc1ab8fdf7dbe386a6e867d20e6d98d9d802abd6387ceff8969041dfd45ec738da7a774c'
